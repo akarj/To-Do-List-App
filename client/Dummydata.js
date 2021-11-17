7799d4a4facff71ad09c7baacad6d8a483d25ab7
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-11-17 17:26:51 +0530

    Dummy data for the front-rnd UI was created!!

---
+++ client/Dummydata.js
@@ -0,0 +1,66 @@
+export default Data = [
+  {
+    id: 1,
+    text: "Do laundry",
+    Status: "pending",
+    subtasks: [
+      {
+        id: 1.1,
+        text: "Pick up the clothes",
+        Status: "completed",
+      },
+      {
+        id: 1.2,
+        text: "Pick up the clothes",
+        Status: "completed",
+      },
+      {
+        id: 1.3,
+        text: "Pick up the clothes",
+        Status: "completed",
+      },
+      {
+        id: 1.4,
+        text: "Bring back the clothes",
+        Status: "pending",
+      },
+      {
+        id: 1.5,
+        text: "Pick up the clothes",
+        Status: "pending",
+      },
+    ],
+  },
+  {
+    id: 2,
+    text: "Do Something Else",
+    Status: "pending",
+    subtasks: [
+      {
+        id: 2.1,
+        text: "Pick up the clothes",
+        Status: "completed",
+      },
+      {
+        id: 2.2,
+        text: "Pick up the clothes",
+        Status: "completed",
+      },
+      {
+        id: 2.3,
+        text: "Pick up the clothes",
+        Status: "completed",
+      },
+      {
+        id: 2.4,
+        text: "Bring back the clothes",
+        Status: "pending",
+      },
+      {
+        id: 2.5,
+        text: "Pick up the clothes",
+        Status: "pending",
+      },
+    ],
+  },
+];
